5bbc8a861eacbd53f69146fac734a57074af873e
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2026-02-20 11:09:50 +0100

    Basic frontend floorplan picker

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>DODO HEAD!</Text>
+      <Button title="Pick an image" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +35,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    flex: 1,
+    marginTop: 20,
+    resizeMode: 'contain',
   },
 });
