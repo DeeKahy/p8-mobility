ab86386ece4f1fcbe7f7726a6e683e78ee76e747
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2026-02-27 13:40:26 +0100

    main page wireframe skelet

--- app/(tabs)/main.tsx
+++ app/(tabs)/main.tsx
@@ -1,12 +1,40 @@
 import { View, Text } from 'react-native';
 import HelloCard from '../../components/HelloCard';
+import Button from '../../components/Button';
+import Dropdown from '../../components/Dropdown';
 export default function Main() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <HelloCard
-        title="Velkommen 👋"
-        subtitle="Dette er din main page"
+    <View style={{  alignItems: 'center', justifyContent: 'center' }}>
+          
+      <div
+      style={{ marginBottom: 40 }}
       />
+        <HelloCard
+        title="Velkommen Til vores Crazy seje app"
+        subtitle=""
+        
+      />
+      <div
+      style={{ marginBottom: 200 }}
+      />
+      <Dropdown
+      selected="My apartments"
+      />
+            <div
+      style={{ marginBottom: 40 }}
+      />
+        <Button
+        label="Upload new floor plan"
+        onPress={() => console.log('Button pressed')}
+      />
+      <div
+      style={{ marginBottom: 40 }}
+      />
+        <Button
+        label="Scan Area"
+        onPress={() => console.log('Button pressed')}
+      />
+
     </View>
   );
 }
