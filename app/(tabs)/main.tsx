663adbbbec82d15ba34dd51fe264173cda21f017
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2026-02-27 12:19:33 +0100

    simpelt eksempel

--- app/(tabs)/main.tsx
+++ app/(tabs)/main.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native';
-
+import HelloCard from '../../components/HelloCard';
 export default function Main() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Main screen</Text>
+    <HelloCard
+        title="Velkommen 👋"
+        subtitle="Dette er din main page"
+      />
     </View>
   );
 }
