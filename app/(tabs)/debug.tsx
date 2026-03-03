d5f196fd8c975691e85104e5b625d074f4c54088
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2026-03-03 11:55:43 +0100

    More Leon fixes. This will Please our great Leon

--- app/(tabs)/debug.tsx
+++ app/(tabs)/debug.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
 
-export default function Bug() {
+export default function DeBug() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Bugg screen</Text>
+      <Text>DeBugg screen</Text>
     </View>
   );
 }
