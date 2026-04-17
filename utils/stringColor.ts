167af6a63fc3b9767361c942b50c86becd18936e
EmilSoegaardLarsen <112959985+EmilSoegaardLarsen@users.noreply.github.com>
2026-04-17 11:35:24 +0200

    Markers are now pie charts
    They show the colors of the area groups according to a hash function in a pie chart format

---
+++ utils/stringColor.ts
@@ -0,0 +1,14 @@
+// Function to turn strings into colors. Thanks, Leon :)
+
+export function hashNameToColor(name: string): string {
+    let hash = 0;
+    name.split("").forEach((char) => {
+        hash = char.charCodeAt(0) + ((hash << 5) - hash);
+    });
+    let color = "#";
+    for (let i = 0; i < 3; i++) {
+        const value = (hash >> (i * 8)) & 0xff;
+        color += value.toString(16).padStart(2, "0");
+    }
+    return color;
+}
