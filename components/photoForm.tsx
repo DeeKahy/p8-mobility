ce39e9351159268af5a72e61425d0c89da96ce84
EmilSoegaardLarsen <112959985+EmilSoegaardLarsen@users.noreply.github.com>
2026-05-15 20:36:07 +0200

    Cleanup after rebase

--- components/photoForm.tsx
+++ components/photoForm.tsx
@@ -63,8 +63,6 @@ export const PhotoFormModal = ({
   date,
   onSubmit,
 }: PhotoFormProps) => {
-  const [open, setOpen] = useState(false);
-  const [showOtherInputForm, setShowOtherInputForm] = useState(false);
   const { showToast } = useOverlays();
 
   useEffect(() => {
