2e1118a16b2e9399b65e5b7d6a28402294fb4a65
EmilSoegaardLarsen <112959985+EmilSoegaardLarsen@users.noreply.github.com>
2026-05-15 20:12:53 +0200

    Big overhaul of photoform and a few other things
    Overlays can now be updated based on provided dependencies, useEffect-style. Provide them to ensure state changes that affect layout etc. are reflected. This system is similar to react-native-root-siblings

--- components/photoForm.tsx
+++ components/photoForm.tsx
@@ -1,24 +1,23 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
+import React, { useState } from "react";
+import { useForm, Controller, ControllerRenderProps } from "react-hook-form";
 //Expected photo data format to make it easier to pass over including only the most necessary. Also works in unison with yup validator.
 import {
   View,
   Image,
   TextInput,
-  Button,
+  TouchableOpacity,
   Text,
   ScrollView,
-  KeyboardAvoidingView,
-  Platform,
-  StyleSheet,
 } from "react-native";
 import DropDownPicker, { ItemType } from "react-native-dropdown-picker";
 import * as yup from "yup";
 
 import FloatingHelpButton from "./FloatingHelpButton";
+import FullscreenImage from "./FullscreenImage";
 import { Overlay } from "../context/Overlays";
 import { styles } from "../css/photoForm";
+import { styles as listStyles } from "../css/photo_list";
 import { PhotoData } from "../models/PhotoFormModel";
 import { hashNameToColor } from "../utils/stringColor";
 //Photo form to take data from index.tsx and opening and closing modal
@@ -33,9 +32,9 @@ type PhotoFormProps = {
 //Form validator
 const schema = yup
   .object({
-    photoUri: yup.string().required("Photo is required"),
-    areaGroup: yup.string().required("Area group is required"),
-    pictureName: yup.string().required("Picture name is required"),
+    photoUri: yup.string().required("Image is required"),
+    areaGroup: yup.string().required("Group is required"),
+    pictureName: yup.string().required("Image title is required"),
     dateTaken: yup.string().required("Date is required"),
     description: yup.string().default(""),
   })
@@ -59,16 +58,13 @@ export const PhotoFormModal = ({
   date,
   onSubmit,
 }: PhotoFormProps) => {
-  const [open, setOpen] = useState(false);
-  const [showOtherInputForm, setShowOtherInputForm] = useState(false);
-
   //Is used for controlling variables (e.g. when we update a value the yup resolver will take note of that).
   //Also for including errors in yup validator based on schema provided above, and also handling submit button.
   const {
     control,
     handleSubmit,
     reset,
-    formState: { errors, isLoading },
+    formState: { errors, isLoading, isSubmitting },
   } = useForm<PhotoData>({
     // Specififying what our form is gonna look like
     defaultValues: {
@@ -90,156 +86,191 @@ export const PhotoFormModal = ({
     "Other",
   ];
 
-  const [items] = useState<ItemType<string>[]>(
-    names.map((s: string) => ({
-      label: s,
-      value: s,
-      icon: () => generateIcon(s),
-    }))
+  const makeItem = (s: string) => ({
+    label: s,
+    value: s,
+    icon: () => generateIcon(s),
+  });
+
+  const [items, setItems] = useState<ItemType<string>[]>(
+    names.map((s: string) => makeItem(s))
   );
 
+  const [fullscreenImage, setFullscreenImage] = useState("");
   return (
-    <Overlay style={StyleSheet.absoluteFill} animationType="slide">
+    <Overlay
+      style={styles.fullscreenOverlay}
+      animationType="slide"
+      dependencies={[errors, fullscreenImage]}
+    >
       <FloatingHelpButton />
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-      >
-        <ScrollView style={[styles.container, styles.fullscreenOverlay]}>
-          <View>
-            <View style={styles.formCard}>
-              <Text>Picture:</Text>
-              <View style={styles.imageContainer}>
-                <Image source={{ uri: photoUri }} style={styles.image} />
-              </View>
-              {errors.photoUri && (
-                <Text style={styles.error}>{errors.photoUri.message}</Text>
+      <View style={{ flex: 1 }}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={{ padding: 20 }}
+        >
+          <View style={styles.formCard}>
+            <Text>Tap to enlarge:</Text>
+            <TouchableOpacity
+              style={styles.imageContainer}
+              onPress={() => {
+                setFullscreenImage(photoUri);
+              }}
+            >
+              <Image source={{ uri: photoUri }} style={styles.image} />
+            </TouchableOpacity>
+            {errors.photoUri ? (
+              <Text style={styles.error}>{errors.photoUri.message}</Text>
+            ) : null}
+
+            <Text>Image title:</Text>
+            {/* Tracking input using control for the form  */}
+            <Controller
+              control={control}
+              name="pictureName"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  style={styles.input}
+                  placeholder="Picture name..."
+                  value={value}
+                  onChangeText={onChange}
+                />
               )}
+            />
+            {errors.pictureName ? (
+              <Text style={styles.error}>{errors.pictureName.message}</Text>
+            ) : null}
 
-              <Text>Picture name:</Text>
-              {/* Tracking input using control for the form  */}
-              <Controller
-                control={control}
-                name="pictureName"
-                render={({ field: { onChange, value } }) => (
-                  <TextInput
-                    style={styles.input}
-                    placeholder="Picture name..."
-                    value={value}
-                    onChangeText={onChange}
-                  />
-                )}
-              />
-              {errors.pictureName && (
-                <Text style={styles.error}>{errors.pictureName.message}</Text>
+            <Text>Group:</Text>
+            <Controller
+              control={control}
+              name="areaGroup"
+              render={({ field }) => (
+                <GroupPicker field={field} items={items} setItems={setItems} />
               )}
+            />
+            {errors.areaGroup ? (
+              <Text style={styles.error}>{errors.areaGroup.message}</Text>
+            ) : null}
 
-              <Text>Area group:</Text>
-              <Controller
-                control={control}
-                name="areaGroup"
-                render={({ field: { onChange, value } }) => (
-                  <View>
-                    <DropDownPicker
-                      open={open}
-                      value={value}
-                      items={items}
-                      listMode="SCROLLVIEW"
-                      setOpen={setOpen}
-                      setValue={(currentValue) => {
-                        const newValue = currentValue(value);
-                        setShowOtherInputForm(newValue === "Other");
-                        onChange(newValue);
-                      }}
-                      placeholder="Select area group"
-                    />
-                    {showOtherInputForm && (
-                      <TextInput
-                        style={styles.input}
-                        placeholder="Enter area group..."
-                        value={value}
-                        onChangeText={onChange}
-                      />
-                    )}
-                  </View>
-                )}
-              />
-              {errors.areaGroup && (
-                <Text style={styles.error}>{errors.areaGroup.message}</Text>
+            <Text>Description:</Text>
+            <Controller
+              control={control}
+              name="description"
+              render={({ field: { value, onChange } }) => (
+                <TextInput
+                  multiline
+                  numberOfLines={5}
+                  style={styles.textarea}
+                  value={value}
+                  onChangeText={onChange}
+                />
               )}
-
-              <Text>Description:</Text>
-              <Controller
-                control={control}
-                name="description"
-                render={({ field: { value, onChange } }) => (
-                  <TextInput
-                    multiline
-                    numberOfLines={5}
-                    style={styles.textarea}
-                    value={value}
-                    onChangeText={onChange}
-                  />
-                )}
-              />
-              <Text>Date:</Text>
-              <Controller
-                control={control}
-                name="dateTaken"
-                render={({ field: { value } }) => (
-                  <TextInput
-                    style={styles.dateInput}
-                    value={date}
-                    placeholder="YYYY-MM-DD"
-                    editable={false}
-                  />
-                )}
-              />
-              {errors.dateTaken && (
-                <Text style={styles.error}>{errors.dateTaken.message}</Text>
+            />
+            <Text>Date:</Text>
+            <Controller
+              control={control}
+              name="dateTaken"
+              render={() => (
+                <TextInput
+                  style={styles.dateInput}
+                  value={date}
+                  placeholder="YYYY-MM-DD"
+                  editable={false}
+                />
               )}
-            </View>
+            />
+            {errors.dateTaken ? (
+              <Text style={styles.error}>{errors.dateTaken.message}</Text>
+            ) : null}
+          </View>
+          <View style={styles.buttonContainer}>
+            <View style={styles.buttonRow}>
+              <TouchableOpacity
+                style={[
+                  listStyles.card,
+                  { width: "40%", backgroundColor: "#19ae75" },
+                ]}
+                disabled={isLoading || isSubmitting}
+                onPress={handleSubmit(
+                  (data) => {
+                    console.log("Submit");
+                    onSubmit(data);
+                    reset({
+                      photoUri,
+                      dateTaken: date,
+                      pictureName: "",
+                      areaGroup: "",
+                      description: "",
+                    });
+                  },
+                  (errors) => {
+                    console.log("Submit blocked by validation.");
+                  }
+                )}
+              >
+                <Text style={styles.buttonText}>Done</Text>
+              </TouchableOpacity>
 
-            <View style={styles.buttonContainer}>
-              <View style={styles.buttonRow}>
-                <View style={styles.buttonWrapper}>
-                  <Button
-                    title="Done"
-                    disabled={isLoading}
-                    onPress={handleSubmit((data) => {
-                      onSubmit(data);
-                      reset({
-                        photoUri,
-                        dateTaken: date,
-                        pictureName: "",
-                        areaGroup: "",
-                        description: "",
-                      });
-                    })}
-                  />
-                </View>
-
-                <View style={[styles.buttonWrapper]}>
-                  <Button
-                    title="Cancel"
-                    color="red"
-                    onPress={() => {
-                      onSkip();
-                      reset({
-                        photoUri,
-                        dateTaken: date,
-                        pictureName: "",
-                        areaGroup: "",
-                        description: "",
-                      });
-                    }}
-                  />
-                </View>
-              </View>
+              <TouchableOpacity
+                style={[
+                  listStyles.card,
+                  { width: "40%", backgroundColor: "#ff3355" },
+                ]}
+                disabled={isLoading || isSubmitting}
+                onPress={() => {
+                  onSkip();
+                  reset({
+                    photoUri,
+                    dateTaken: date,
+                    pictureName: "",
+                    areaGroup: "",
+                    description: "",
+                  });
+                }}
+              >
+                <Text style={styles.buttonText}>Skip</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </ScrollView>
-      </KeyboardAvoidingView>
+      </View>
+      {fullscreenImage ? (
+        <FullscreenImage uri={fullscreenImage} setUri={setFullscreenImage} />
+      ) : null}
     </Overlay>
   );
 };
+
+// We need to separate the group picker from the rest of the form.
+// Otherwise, it will reset (and close) when any part of the form is interacted with, as far as I can tell.
+interface GroupPickerProps {
+  field: ControllerRenderProps<PhotoData, "areaGroup">;
+  items: ItemType<string>[];
+  setItems: React.Dispatch<React.SetStateAction<ItemType<string>[]>>;
+}
+
+const GroupPicker = React.memo(
+  ({ field, items, setItems }: GroupPickerProps) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+      <DropDownPicker
+        open={open}
+        value={field.value}
+        items={items}
+        setOpen={setOpen}
+        setItems={setItems}
+        setValue={(callback) => {
+          field.onChange(callback(field.value));
+        }}
+        searchable
+        closeAfterSelecting
+        closeOnBackPressed
+        listMode="SCROLLVIEW"
+        placeholder="Select group"
+        searchPlaceholder="Enter group"
+      />
+    );
+  }
+);
