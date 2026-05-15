36ea348c630a6324ae5e22bd6c209908c89090b9
EmilSoegaardLarsen <112959985+EmilSoegaardLarsen@users.noreply.github.com>
2026-05-15 12:34:18 +0200

    Fix for strange interface with AR

--- components/SaveModal.tsx
+++ components/SaveModal.tsx
@@ -3,56 +3,51 @@ import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 import { Overlay } from "../context/Overlays";
 
 type SaveFormModalProps = {
-  visible: boolean;
   onClose: () => void;
   onSave: () => void;
   onSaveNext: () => void;
 };
 
 export const SaveFormModal = ({
-  visible,
   onClose,
   onSave,
   onSaveNext,
 }: SaveFormModalProps) => {
-  if (!visible) return null;
   return (
     <Overlay style={styles.overlay} animationType="fade">
-      <>
-        <View style={styles.card}>
-          <Text style={styles.title}>Save Floorplan</Text>
-          <Text style={styles.message}>
-            Choose what to do after saving this room.
-          </Text>
+      <View style={styles.card}>
+        <Text style={styles.title}>Save Floorplan</Text>
+        <Text style={styles.message}>
+          Choose what to do after saving this room.
+        </Text>
 
-          <View style={styles.actionColumn}>
-            <TouchableOpacity
-              onPress={onSave}
-              style={[styles.button, styles.primaryButton]}
-            >
-              <Text style={[styles.buttonText, styles.primaryButtonText]}>
-                Save and review floorplan
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={onSaveNext}
-              style={[styles.button, styles.primaryButton]}
-            >
-              <Text style={[styles.buttonText, styles.primaryButtonText]}>
-                Save and measure next room
-              </Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.actionColumn}>
           <TouchableOpacity
-            onPress={onClose}
-            style={[styles.button, styles.secondaryButton]}
+            onPress={onSave}
+            style={[styles.button, styles.primaryButton]}
           >
-            <Text style={[styles.buttonText, styles.secondaryButtonText]}>
-              Cancel
+            <Text style={[styles.buttonText, styles.primaryButtonText]}>
+              Save and review floorplan
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={onSaveNext}
+            style={[styles.button, styles.primaryButton]}
+          >
+            <Text style={[styles.buttonText, styles.primaryButtonText]}>
+              Save and measure next room
             </Text>
           </TouchableOpacity>
         </View>
-      </>
+        <TouchableOpacity
+          onPress={onClose}
+          style={[styles.button, styles.secondaryButton]}
+        >
+          <Text style={[styles.buttonText, styles.secondaryButtonText]}>
+            Cancel
+          </Text>
+        </TouchableOpacity>
+      </View>
     </Overlay>
   );
 };
